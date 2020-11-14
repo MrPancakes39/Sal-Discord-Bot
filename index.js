230e7789910c057c0f9d05c2c96bff55ce2b22fc
MrPancakes39 <salmannhassan39@gmail.com>
2020-11-14 13:54:04 +0200

    Made a Discord Bot

---
+++ index.js
@@ -0,0 +1,75 @@
+console.log("Beep boop! 🤖");
+const fetch = require("node-fetch");
+require("dotenv").config();
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.login(`${process.env.BOT_TOKEN}`);
+
+client.on("ready", () => console.log("I'm Ready! >.<"));
+client.on("message", (msg) => {
+  // make evrything lower case and check if it starts with sal.
+  let content = msg.content.toLowerCase();
+  if (!(/^sal/g.test(content))) return;
+
+  // parses the content.
+  const withoutPrefix = content.slice(4);
+  const split = withoutPrefix.split(/ +/);
+  const cmd = split[0];
+  const args = split.slice(1);
+
+  gotCMD(msg, cmd, args);
+});
+
+async function gotCMD(msg, cmd, args) {
+  switch (cmd) {
+    case "":
+      msg.channel.send("It'sa me Sal-kun");
+      break;
+
+    case "hug":
+      let user;
+      if (args[0]) {
+        user = getUserFromMention(args[0]);
+        if (!user) {
+          return msg.reply("Please use a proper mention, thank you.");
+        }
+      } else {
+        user = {};
+        user.username = "themselves";
+      }
+
+      let url = await getGIF();
+      const hugEmbed = new Discord.MessageEmbed()
+        .setColor("#0099ff")
+        .setTitle(`${msg.author.username} hugged ${user.username}`)
+        .setImage(url);
+      msg.channel.send(hugEmbed);
+      break;
+
+    default:
+      msg.reply("Unfortunately It's an Unknown Command.")
+  }
+}
+
+async function getGIF() {
+  // let res = await fetch(`http://api.giphy.com/v1/gifs/random?tag=anime+hug&api_key={process.env.GIPHY_KEY}&rating=g`);
+  let res = await fetch(`https://api.tenor.com/v1/random?q=anime+hug&key=${process.env.TENOR_KEY}&limit=1`)
+  let data = await res.json();
+  // return data["data"]["image_original_url"];
+  return data["results"][0]["media"][0]["gif"]["url"];
+}
+
+function getUserFromMention(mention) {
+  if (!mention) return;
+
+  if (mention.startsWith("<@") && mention.endsWith(">")) {
+    mention = mention.slice(2, -1);
+
+    if (mention.startsWith("!")) {
+      mention = mention.slice(1);
+    }
+
+    return client.users.cache.get(mention);
+  }
+}
