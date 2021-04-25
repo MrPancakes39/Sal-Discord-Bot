d9834badc9a518a87ef8376f12941098255f2e21
MrPancakes39 <salmannhassan39@gmail.com>
2021-04-25 20:41:00 +0300

    Load Help Menu from text file

--- commands/help.js
+++ commands/help.js
@@ -1,37 +1,22 @@
+const fs = require("fs");
 const Discord = require("discord.js");
 
-module.exports = function (msg) {
+module.exports = function(msg) {
     let descrption =
         "To use a command type: `sal <command name>` for example: `sal help`\n" +
         "You can also mention me to get help message.\n\u200B";
+    const helpText = fs.readFileSync(__dirname + "/../assets/help.txt", "utf-8");
+    const fields = helpText.split("---").map(field => {
+        let temp = field.trim().split(",\n");
+        return {
+            name: temp[0],
+            value: temp[1]
+        }
+    });
     const helpEmbed = new Discord.MessageEmbed()
         .setColor("#f89e4f")
         .setAuthor(`Sal-kun Bot's Commands:`, `https://i.imgur.com/rzvO3bA.png`)
         .setDescription(descrption)
-        .addFields({
-            name: ":tools: help",
-            value: "Shows help message"
-        }, {
-            name: ":hugging: hug",
-            value: "`hug` to hug yourself\n`hug <username>` to hug a user"
-        }, {
-            name: ":stuck_out_tongue_closed_eyes: gif",
-            value: "`gif term` to give you a GIF"
-        }, {
-            name: ":cat: aww",
-            value: "Will make you go 'aww' from r/aww"
-        }, {
-            name: ":joy: memes",
-            value: "Gives you some memes from r/memes"
-        }, {
-            name: ":cry: cry",
-            value: "Why you cry?"
-        }, {
-            name: ":game_die: play",
-            value: "`play roshambo` to play rock, paper, scissors.\n`More Coming Soon...`"
-        }, {
-            name: ":ping_pong: ping",
-            value: "Shows ping results"
-        })
+        .addFields(...fields);
     msg.channel.send(helpEmbed);
 }
